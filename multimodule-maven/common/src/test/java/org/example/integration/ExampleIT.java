96a6a4cbaa8b23888199bb762af2c515c556b846
Viktor <vitya.malahov.95@gmail.com>
2023-02-05 20:10:33 +0200

    first homework

---
+++ multimodule-maven/common/src/test/java/org/example/integration/ExampleIT.java
@@ -0,0 +1,13 @@
+package org.example.integration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExampleIT {
+
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue(true);
+    }
+}
